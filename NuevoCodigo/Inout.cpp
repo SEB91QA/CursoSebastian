1f16c0f9e9f109dfd9edacc0a21d70ecd8e8aeda
asegura4488 <asegura4488@gmail.com>
2020-06-08 20:13:06 -0500

    l

--- NuevoCodigo/Inout.cpp
+++ NuevoCodigo/Inout.cpp
@@ -1,6 +1,13 @@
 
 #include<iostream>
 
+int par=0;
+
+void Print(){
+	std::cout << par << std::endl;  
+	par++;
+}
+
 int main(int argc, char *argv[])
 {
 
@@ -13,9 +20,13 @@ int main(int argc, char *argv[])
     float param2 = std::stof(argv[2]);
     std::string param3 = argv[3];
 
+    par = param1;
 
     std::cout << " "  << param1 << " "  << param2 << " " << param3 << std::endl;
 
+
+    for(int i = 0; i < 9; i++)
+    	Print();
 
     return 0;
 
