54cdff758d63855fecf5e697d8d7f186f432a30b
asegura4488 <asegura4488@gmail.com>
2020-06-29 21:15:27 -0500

    horas

--- rebin.C
+++ rebin.C
@@ -1,8 +1,8 @@
 
 TH1D* cambiarbin(TH1D *h_old){
  
-  double BINS[]={0,20,100,500};
-  int Nbins = 3; 
+  double BINS[]={0,10};
+  int Nbins = 1; 
 
   TH1D *h_new = (TH1D*)h_old->Clone("h_new");	
   h_new->Sumw2();
@@ -12,25 +12,31 @@ TH1D* cambiarbin(TH1D *h_old){
   Int_t new_nbins = h_new->GetXaxis()->GetNbins();
 
   for (int b = 1; b <= new_nbins; b++ ){
-	  cout << b << endl;
-    Double_t Right_edge_new = h_new->GetXaxis()->GetBinLowEdge(b)+h_new->GetXaxis()->GetBinWidth(b);
-    Double_t Edge_new = h_new->GetXaxis()->GetBinLowEdge(b);
+	  cout << " bin del nuevo " <<  b << endl;
+    Double_t Right_edge_new = h_new->GetXaxis()->GetBinUpEdge(b); 
+    //Double_t Right_edge_new = h_new->GetXaxis()->GetBinLowEdge(b)+h_new->GetXaxis()->GetBinWidth(b);
+    Double_t Left_edge_new = h_new->GetXaxis()->GetBinLowEdge(b);
 
-    cout << Edge_new << "   " << Right_edge_new << endl;
+    cout << Left_edge_new << "   " << Right_edge_new << endl;
     Double_t Num_back = 0.;
     Double_t error_back = 0.;
 
       for (int i = 1; i <= old_nbins; i++ ){
-	      cout << "old "  <<i << endl;
+	      cout << "bin del viejo "  <<i << endl;
       Double_t Right_edge_old = h_old->GetXaxis()->GetBinLowEdge(i)+h_old->GetXaxis()->GetBinWidth(i);
-              cout << Right_edge_old << endl;
-      if((Right_edge_old <= Right_edge_new) && (Right_edge_old > Edge_new)){ //Relevant Condition
+              
+      
+       cout << " derecho del viejo "   << Right_edge_old << endl;
 
+      if((Right_edge_old <= Right_edge_new) && (Right_edge_old > Left_edge_new)){ //Relevant Condition
+        cout << " si pasa " << endl; 
         Num_back += h_old->GetBinContent(i);
         error_back += TMath::Power( h_old->GetBinError(i), 2 ) ;
 
       }
-      else continue;
+      else {  cout << " no esta contenido " << endl; 
+	      continue;
+      }
 
       h_new->SetBinContent(b, Num_back);
       h_new->SetBinError(b, TMath::Sqrt(error_back));
@@ -47,10 +53,10 @@ TH1D* cambiarbin(TH1D *h_old){
 
 void rebin(){
 
- TH1D *h1 = new TH1D("h1","h1", 500, 0., 500.);
+ TH1D *h1 = new TH1D("h1","h1", 5 , 0., 10.);
 
  for(int i = 0; i < 10000; i++){
- h1->Fill(gRandom->Gaus(200.,200.));
+ h1->Fill(gRandom->Gaus(0.,10.));
  }
  h1->Draw();
 
@@ -60,14 +66,16 @@ void rebin(){
 
  cout << h2->GetBinError(1) << " " << h2->GetBinError(2) << endl;
 
- TH1D *rebinh1 = cambiarbin(h2); 
+ TH1D *rebinh1 = cambiarbin(h1); 
 
  cout << rebinh1->GetBinError(1) << endl;
 
  rebinh1->SetLineColor(2);
- rebinh1->Draw("");
 
- h2->Draw("same");
+ rebinh1->Draw("");
+ h1->Draw("same");
+ //h1->Draw("same");
+
 }
 
 
